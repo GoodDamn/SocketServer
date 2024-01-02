7fbf2c6e597f74ad7c69bda7ca307a0d43fcf135
GoodDamn <76108467+GoodDamn@users.noreply.github.com>
2024-01-02 21:25:50 +0300

    Avoiding deprecated methods

--- app/src/main/java/good/damn/filesharing/activities/ServerActivity.kt
+++ app/src/main/java/good/damn/filesharing/activities/ServerActivity.kt
@@ -1,7 +1,14 @@
 package good.damn.filesharing.activities
 
 import android.annotation.SuppressLint
+import android.net.ConnectivityManager
+import android.net.LinkProperties
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import android.net.wifi.WifiInfo
 import android.net.wifi.WifiManager
+import android.os.Build
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -24,9 +31,10 @@ import java.net.ServerSocket
 import java.net.Socket
 import java.nio.ByteOrder
 
+@OptIn(ExperimentalUnsignedTypes::class)
 class ServerActivity
     : AppCompatActivity(),
-      Server.ServerListener {
+    Server.ServerListener {
 
     private val TAG = "ServerActivity"
 
@@ -37,45 +45,49 @@ class ServerActivity
         super.onCreate(savedInstanceState)
 
         val port = 1250
-        val ip = getLanIP()
 
         val server = Server(port)
         server.setOnServerListener(this)
 
         val contentLauncher = ContentLauncher(this) { uri ->
             val data = FileUtils
-                .read(uri,this)
+                .read(uri, this)
 
             if (data == null) {
                 Toast.makeText(
                     this,
                     "Something went wrong",
-                    Toast.LENGTH_SHORT)
+                    Toast.LENGTH_SHORT
+                )
                     .show()
                 return@ContentLauncher
             }
 
             val p = uri!!.path!!
             val t = "primary:"
-            val filePath = p.substring(p.indexOf(t)+t.length)
+            val filePath = p.substring(p.indexOf(t) + t.length)
 
             val nameIndex = filePath.lastIndexOf("/")
 
             val fileName = if (nameIndex == -1)
-                                filePath
-                           else filePath.substring(nameIndex+1)
+                filePath
+            else filePath.substring(nameIndex + 1)
 
-            server.setResponse(data,fileName)
+            server.setResponse(data, fileName)
             Toast.makeText(
                 this,
                 "FILE IS PREPARED $fileName",
-                Toast.LENGTH_SHORT)
+                Toast.LENGTH_SHORT
+            )
                 .show()
         }
 
         val textViewIP = TextView(this)
-        textViewIP.text = "Host: $ip\nPort: $port"
         textViewIP.textSize = 18.0f
+
+        getLanIP {
+            textViewIP.text = "Host: $it\nPort: $port"
+        }
 
         val btnCreate = Button(this)
         btnCreate.text = "Create server"
@@ -102,14 +114,14 @@ class ServerActivity
         rootLayout.gravity = Gravity.CENTER
         rootLayout.orientation = LinearLayout.VERTICAL
 
-        rootLayout.addView(textViewIP,-1,-2)
-        rootLayout.addView(btnCreate,-1,-2)
-        rootLayout.addView(btnDrop,-1,-2)
-        rootLayout.addView(editTextMsg, -1,-2)
-        rootLayout.addView(btnResponseFile, -1,-2)
-        rootLayout.addView(textViewMsg, -1,-1)
+        rootLayout.addView(textViewIP, -1, -2)
+        rootLayout.addView(btnCreate, -1, -2)
+        rootLayout.addView(btnDrop, -1, -2)
+        rootLayout.addView(editTextMsg, -1, -2)
+        rootLayout.addView(btnResponseFile, -1, -2)
+        rootLayout.addView(textViewMsg, -1, -1)
 
-        editTextMsg.addTextChangedListener(object: TextWatcher {
+        editTextMsg.addTextChangedListener(object : TextWatcher {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 server.setResponseText(s.toString())
             }
@@ -171,7 +183,8 @@ class ServerActivity
         val msg = FileUtils.writeToDoc(
             fileName,
             data,
-            offset)
+            offset
+        )
 
         if (msg != null) {
             msgr.addMessage("SAVE PROCESS::EXCEPTION\n$msg")
@@ -204,22 +217,61 @@ class ServerActivity
         msgr.addMessage("Server is dropped")
     }
 
-    @OptIn(ExperimentalUnsignedTypes::class)
-    private fun getLanIP(): String {
-        val wifi = applicationContext.getSystemService(WIFI_SERVICE)
-                as WifiManager
-
-        var ip = wifi.connectionInfo.ipAddress
-
+    private fun getIpString(
+        ip: Int
+    ): String {
         Log.d(TAG, "getLanIP: WIFI_IP: $ip")
+        val arr = ByteUtils.integer(
+            if (ByteOrder.nativeOrder()
+                    .equals(ByteOrder.LITTLE_ENDIAN)
+            ) Integer.reverseBytes(ip)
+            else ip
+        )
+        return "${arr[0]}.${arr[1]}.${arr[2]}.${arr[3]}"
+    }
 
-        if (ByteOrder.nativeOrder()
-                .equals(ByteOrder.LITTLE_ENDIAN)
-        ) {
-            ip = Integer.reverseBytes(ip)
+    private fun getLanIP(
+        onGetIP: (String) -> Unit
+    ) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val connectManager = applicationContext.getSystemService(CONNECTIVITY_SERVICE)
+                    as ConnectivityManager
+
+            val request = NetworkRequest.Builder()
+                .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+                .build()
+
+            val networkCallback = object : ConnectivityManager.NetworkCallback() {
+                override fun onAvailable(network: Network) {
+                    Log.d(TAG, "onAvailable: ")
+                    super.onAvailable(network)
+                }
+
+                override fun onLinkPropertiesChanged(
+                    network: Network,
+                    link: LinkProperties
+                ) {
+                    val ip = link.linkAddresses.toString()
+                    Log.d(TAG, "onLinkPropertiesChanged: $ip")
+                    onGetIP(ip)
+                    super.onLinkPropertiesChanged(network, link)
+                }
+
+                override fun onUnavailable() {
+                    Log.d(TAG, "onUnavailable: ")
+                    super.onUnavailable()
+                }
+            }
+
+            connectManager.apply {
+                requestNetwork(request, networkCallback)
+                registerNetworkCallback(request, networkCallback)
+            }
+            return
         }
 
-        val arr = ByteUtils.integer(ip)
-        return "${arr[0]}.${arr[1]}.${arr[2]}.${arr[3]}"
+        val wifi = applicationContext.getSystemService(WIFI_SERVICE)
+                as WifiManager
+        onGetIP(getIpString(wifi.connectionInfo.ipAddress))
     }
 }

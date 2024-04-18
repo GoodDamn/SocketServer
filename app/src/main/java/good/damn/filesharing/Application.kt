194dd30b64233891bb424e557b60d1777e6dabfc
GoodDamn <76108467+GoodDamn@users.noreply.github.com>
2024-04-18 14:20:04 +0300

    App SSLContext

--- app/src/main/java/good/damn/filesharing/Application.kt
+++ app/src/main/java/good/damn/filesharing/Application.kt
@@ -4,6 +4,10 @@ import android.content.Context
 import android.widget.Toast
 import java.nio.Buffer
 import java.nio.charset.Charset
+import java.security.KeyStore
+import javax.net.ssl.KeyManagerFactory
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManagerFactory
 
 class Application
 : android.app.Application() {
@@ -12,6 +16,8 @@ class Application
         val BUFFER_MB = ByteArray(1024*1024)
         val CHARSET = Charset.forName("UTF-8")
         val CHARSET_ASCII = Charset.forName("US-ASCII")
+
+        lateinit var SSL: SSLContext
 
         fun showMessage(
             msg: String,
@@ -24,6 +30,56 @@ class Application
             ).show()
         }
 
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val password = "123456"
+            .toCharArray()
+
+        val keyStore = KeyStore.getInstance("JKS")
+        keyStore.load(
+            resources.openRawResource(
+                R.raw.multi
+            ),
+            password
+        )
+
+        val trustStore = KeyStore.getInstance("JKS")
+        trustStore.load(
+            resources.openRawResource(
+                R.raw.multi_trust
+            ),
+            password
+        )
+
+        val keyManager = KeyManagerFactory.getInstance(
+            KeyManagerFactory.getDefaultAlgorithm()
+        )
+
+        keyManager.init(
+            keyStore,
+            password
+        )
+
+        val trustManager = TrustManagerFactory.getInstance(
+            TrustManagerFactory.getDefaultAlgorithm()
+        )
+
+        trustManager.init(
+            trustStore
+        )
+
+        SSL = SSLContext.getInstance(
+            "TLS"
+        )
+
+        SSL.init(
+            keyManager.keyManagers,
+            trustManager.trustManagers,
+            null
+        )
     }
 
 }

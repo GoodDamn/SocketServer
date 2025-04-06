ef62164c9f58ca149a561debdcf10190616b6147
GoodDamn <grigorydum@gmail.com>
2025-04-06 16:50:47 +0300

    HTTPResponseService: generate list files for directories

--- app/src/main/java/good/damn/filesharing/services/network/request/HTTPResponseService.kt
+++ app/src/main/java/good/damn/filesharing/services/network/request/HTTPResponseService.kt
@@ -1,5 +1,6 @@
 package good.damn.filesharing.services.network.request
 
+import android.util.Log
 import good.damn.filesharing.Application
 import good.damn.filesharing.utils.FileUtils
 import java.io.File
@@ -16,10 +17,30 @@ class HTTPResponseService {
             val doc = FileUtils
                 .getDocumentsFolder()
 
+            Log.d(TAG, "executeStream: $path")
             val file = File("$doc/$path")
             val fileLen = file.length()
 
-            if (file.isDirectory || !file.exists() || fileLen == 0L) {
+            if (file.isDirectory) {
+                val content = generateHtmlListFiles(
+                    file
+                ).toByteArray(
+                    Application.CHARSET
+                )
+
+                out.write(
+                    getHeaderDocument(
+                        content.size
+                    )
+                )
+
+                out.write(
+                    content
+                )
+                return
+            }
+
+            if (!file.exists() || fileLen == 0L) {
                 out.write(
                     getHeaderError()
                 )
@@ -55,37 +76,60 @@ class HTTPResponseService {
                 out
             )
         }
-
-        private fun getHeaderFile(
-            contentSize: Int,
-            fileName: String
-        ): ByteArray {
-            return (
-                "HTTP/1.0 200 OK\r\n" +
-                    "Content-Length: $contentSize\r\n" +
-                    "Content-Type: application/octet-stream;\r\n" +
-                    "Content-Disposition: inline; filename=\"$fileName\"\r\n\r\n"
-                ).toByteArray(Application.CHARSET)
-        }
-
-        private fun getHeaderError(): ByteArray {
-            return (
-                "HTTP/1.0 404 Not Found\r\n"+
-                    "Content-Length: 9\r\n"+
-                    "Content-Type: text/html; \r\n"+
-                    "Date: Mon, 29 Jan 2024 17:09:46 GMT\r\n\r\nNot found"
-                ).toByteArray(Application.CHARSET)
-        }
-
-        private fun getHeaderDocument(
-            contentSize: Int
-        ): ByteArray {
-            return (
-                "HTTP/1.0 200 OK\r\n" +
-                    "Content-Length: $contentSize\r\n" +
-                    "Content-Type: text/html; charset=UTF-8\r\n" +
-                    "Date: Mon, 29 Jan 2024 17:09:46 GMT\r\n\r\n"
-                ).toByteArray(Application.CHARSET)
-        }
     }
+}
+
+private inline fun generateHtmlListFiles(
+    dir: File
+): String {
+    val builder = StringBuilder()
+
+    builder.append(
+        "<html><ul>"
+    )
+
+    dir.listFiles()?.forEach {
+        builder.append("<li>")
+        builder.append("<a href=\"${it.name}\">")
+        builder.append(it.name)
+        builder.append("</a></li>")
+    }
+
+    builder.append(
+        "</ul></html>"
+    )
+
+    return builder.toString()
+}
+
+private inline fun getHeaderFile(
+    contentSize: Int,
+    fileName: String
+): ByteArray {
+    return (
+        "HTTP/1.0 200 OK\r\n" +
+            "Content-Length: $contentSize\r\n" +
+            "Content-Type: application/octet-stream;\r\n" +
+            "Content-Disposition: inline; filename=\"$fileName\"\r\n\r\n"
+        ).toByteArray(Application.CHARSET)
+}
+
+private inline fun getHeaderError(): ByteArray {
+    return (
+        "HTTP/1.0 404 Not Found\r\n"+
+            "Content-Length: 9\r\n"+
+            "Content-Type: text/html; \r\n"+
+            "Date: Mon, 29 Jan 2024 17:09:46 GMT\r\n\r\nNot found"
+        ).toByteArray(Application.CHARSET)
+}
+
+private inline fun getHeaderDocument(
+    contentSize: Int
+): ByteArray {
+    return (
+        "HTTP/1.0 200 OK\r\n" +
+            "Content-Length: $contentSize\r\n" +
+            "Content-Type: text/html; charset=UTF-8\r\n" +
+            "Date: Mon, 29 Jan 2024 17:09:46 GMT\r\n\r\n"
+        ).toByteArray(Application.CHARSET)
 }
